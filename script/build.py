129d15e8a0173e9d4776be7b5acda15ec672e0ee
Trim Bresilla <trim.bresilla@gmail.com>
2025-04-13 15:30:54 +0200

    fix: use real hex for build.py script

--- script/build.py
+++ script/build.py
@@ -1,8 +1,8 @@
 Import("env")
 
 # nodeid in ipv6 format
-ipv6_first = 65
-ipv6_last = 65
+ipv6_first = 0x65
+ipv6_last = 0x65
 
 # env.Append(CCFLAGS=["MY_NODE_ID=32"])
 env.Append(CPPDEFINES=["IPV6_FIRST=0x%x" % ipv6_first, "IPV6_LAST=0x%x" % ipv6_last])
