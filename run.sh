356961308e1a22193cf1eecfef35b2e96d5fadec
Trim Bresilla <trim.bresilla@gmail.com>
2025-04-13 19:24:45 +0200

    chore: more QOL in run.sh

--- run.sh
+++ run.sh
@@ -15,10 +15,17 @@ elif ! [ -x "$(command -v pio)" ]; then
 elif ! [ -x "$(command -v jq)" ]; then
   echo 'Error: jq is not installed, install from https://github.com/jqlang/jq'
   exit 1
+elif ! [ -x "$(command -v gum)" ]; then
+  echo 'Error: gum is not installed, install from https://github.com/charmbracelet/gum'
+  exit 1
 fi
 
 get_board() {
-    board=$(pio device list --json-output | jq -c '.[] | select(.hwid != "n/a")' | fzy)
+    board=$(pio device list --json-output | jq -c '.[] | select(.hwid != "n/a")' | gum choose)
+    if [ -z "$board" ]; then
+        echo "No board found"
+        exit 1
+    fi
     hid=$(echo "$board" | jq -r ".hwid" | cut -d" " -f2 | cut -d"=" -f2)
     serial_value=$(echo "$board" | jq -r ".hwid" | cut -d" " -f3 | cut -d"=" -f2)
     env_value=${deviceMap[$hid]:-"unknown"}
@@ -27,41 +34,48 @@ get_board() {
 }
 
 # @cmd build the project
-build_all() {
-    pio -f -c vim run
-}
-
-# @cmd build specific env
+# @alias b
 build() {
-    IFS="|" read board_env board_port board_serial < <(get_board)
-    echo "BOARD_ENV: $board_env"
-    echo "BOARD_PORT: $board_port"
-    echo "BOARD_SERIAL: $board_serial"
-    pio -f -c vim run -e $board_env
+    if [ -z "$BOARD_ENV" ]; then
+        echo "BOARD_ENV is not set, building all"
+        pio -f -c vim run
+    else
+        pio -f -c vim run -e $BOARD_ENV
+    fi
 }
 
-# @cmd upload specific env
+# @cmd upload to board
+# @alias u
 upload() {
     IFS="|" read board_env board_port board_serial < <(get_board)
     export BOARD_ENV=$board_env
     export BOARD_PORT=$board_port
     export BOARD_SERIAL=$board_serial
-    pio -f -c vim run -e $board_env -t upload --upload-port $board_port
+    gum confirm --default "Do you want to upload to device??" && pio -f -c vim run -e $board_env -t upload --upload-port $board_port
 }
 
-# @cmd update project
-update() {
+# @cmd refresh build files
+# @alias r
+refresh() {
     pio project init --ide vim && pio run --target compiledb
 }
 
-# @cmd monitor serial
-# @alias monitor
-serial() {
+# @cmd monitor the serial port
+# @alias m
+monitor() {
     IFS="|" read board_env board_port board_serial < <(get_board)
     export BOARD_ENV=$board_env
     export BOARD_PORT=$board_port
     export BOARD_SERIAL=$board_serial
     pio device monitor --baud 115200 --port $board_port
+}
+
+
+# @cmd test with script/send.py
+# @alias t
+test() {
+    gum input --placeholder="FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF"
+    python3 script/send.py
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
